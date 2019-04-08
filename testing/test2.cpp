a2ed653e38c19fdc31c177c5f6caed46daeec717
mshpotak <mshpotak@ukr.net>
2019-04-08 18:01:30 +0200

    Testing strtok()

--- testing/test2.cpp
+++ testing/test2.cpp
@@ -5,11 +5,18 @@
 
 using namespace std;
 
-int main()
+
+
+int main(int argc, const char* argv[])
 {
-    int a = 0;
-    int b = -1;
-    cout << "1 " << !a << endl;
-    cout << "2 " << !b << endl;
+    char str[] = "{-1 59 2 148235353571.1 15.88 73.39 1.04 0.34 0.10 0.25 148235353571.1 0.028 0.013 0.976 5.329 -1.199 -0.604 -0.200 -0.119 -0.440 1020.78 3 }";
+    char * pch;
+    printf ("Splitting string \"%s\" into tokens:\n",str);
+    pch = strtok (str," {}");
+    while (pch != NULL)
+    {
+    printf ("%s\n",pch);
+    pch = strtok (NULL, " {}");
+    }
     return 0;
 }

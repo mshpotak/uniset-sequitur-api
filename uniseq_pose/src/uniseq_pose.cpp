b9423acb51196fdb689b7e713f311c33015c2a48
mshpotak <mshpotak@ukr.net>
2019-04-30 16:25:29 +0200

    Update on ROS integration

--- uniseq_pose/src/uniseq_pose.cpp
+++ uniseq_pose/src/uniseq_pose.cpp
@@ -1,7 +1,16 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "seq-req-class-v2.hpp"
 
 #include <sstream>
+
+void msg_conv( pose_stamped a, geometry_msgs::PoseStamped b){
+    b.pose.position.x = a.position.x;
+    b.pose.position.y = a.position.y;
+    b.pose.position.z = a.position.z;
+    ros::Time stamp(a.timestamp/1000);
+    b.header.stamp = stamp;
+}
 
 int main( int argc, char *argv[]){
     ros::init( argc, argv, "uniseq_pose");
@@ -9,16 +18,13 @@ int main( int argc, char *argv[]){
     ros::Publisher pose_pub = hand.advertise<geometry_msgs::PoseStamped>("pose",1000);
     ros::Rate loop_rate(10);
     int count = 0;
-    // estabilish connection
-    while(ros::ok()){
-        geometry_msgs::PoseStamped msg;
+    Forward fwd;
+    geometry_msgs::PoseStamped msg;
+    while( ros::ok() ){
 
-        //send msg
-        //receive msg
+        fwd.recv_upd();
         msg.header.seq = count;
-        msg.pose.position.x = 0;
-        msg.pose.position.y = 0;
-        msg.pose.position.z = 0;
+        msg_conv( fwd.pose, msg );
 
         ROS_INFO("count = %d:\tx = %f\ty = %f\tz = %f", msg.header.seq, msg.pose.position.x, msg.pose.position.y, msg.pose.position.z );
         pose_pub.publish(msg);

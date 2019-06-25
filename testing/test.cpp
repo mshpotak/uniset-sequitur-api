c93c7daae01d5ae99038eea4f29c58e36807401d
mshpotak <mshpotak@ukr.net>
2019-06-25 19:10:43 +0200

    Some test

--- testing/test.cpp
+++ testing/test.cpp
@@ -7,25 +7,49 @@
 #include <sys/types.h>
 #include <ifaddrs.h>
 #include <errno.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 using namespace std;
 
 int main()
 {
     int result;
+    int i;
     struct ifaddrs *addr = NULL;
     struct ifaddrs *ifs = NULL;
+    char buff[INET_ADDRSTRLEN];
 
     result = getifaddrs(&ifs);
     if( result == -1 ){
-        perror();
+        perror("getifaddrs error");
         return -1;
     }
     addr = ifs;
     while( addr != NULL ){
-        printf("%s\n", addr->ifa_name);
+        printf("\n%s\t", addr->ifa_name);
+        if(addr->ifa_addr->sa_family == AF_INET){
+            inet_ntop( AF_INET, &(((struct sockaddr_in*)(addr->ifa_addr))->sin_addr.s_addr), buff, INET_ADDRSTRLEN);
+            printf("%s\t", buff);
+            if( strstr(buff, "192.168") ){
+                printf("Found it!\t");
+                result = socket( AF_INET, SOCK_DGRAM, AI_PASSIVE );
+                if( result == -1){
+                    perror("socket error");
+                    return -1;
+                }
+                for( i = 0; i <= 255; i++){
+                    connect()
+
+                }
+            }
+        }
         addr = addr->ifa_next;
     }
+
 
     freeifaddrs( ifs );
     return 0;
--- testing/test2.cpp
+++ testing/test2.cpp
@@ -5,18 +5,14 @@
 
 using namespace std;
 
-
+//char buffer[1024];
 
 int main(int argc, const char* argv[])
 {
     char str[] = "{-1 59 2 148235353571.1 15.88 73.39 1.04 0.34 0.10 0.25 148235353571.1 0.028 0.013 0.976 5.329 -1.199 -0.604 -0.200 -0.119 -0.440 1020.78 3 }";
-    char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str," {}");
-    while (pch != NULL)
-    {
-    printf ("%s\n",pch);
-    pch = strtok (NULL, " {}");
-    }
+    sscanf(str,"{%*d %*d %*d %[^_}]", str);
+    string buff = str;
+    cout << buff << endl;
+
     return 0;
 }
